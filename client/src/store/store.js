b7b63947ac4287880dff98a49f1359ac37e710b1
mickoymouse <mckslvstr@gmail.com>
2021-06-30 03:07:22 +0800

    Added posts reducer.

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from './counter/counterSlice';
+import postsReducer from './Posts/postsSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    Posts: postsReducer,
   },
 });
