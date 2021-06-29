eac64299d70b2388bf9fa0386d943eb17e6bf9ae
mickoymouse <mckslvstr@gmail.com>
2021-06-30 04:11:59 +0800

    Changed database name.

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use("/posts", postRoutes);
 
 const CONNECTION_URL =
-  "mongodb+srv://micko:P@55w0rd123@memories.2mocc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  "mongodb+srv://micko:P@55w0rd123@memories.2mocc.mongodb.net/memories?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
