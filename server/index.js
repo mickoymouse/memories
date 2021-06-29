7441d5ed286ebcb706aec7dc3f21ec5e53961d95
mickoymouse <mckslvstr@gmail.com>
2021-06-30 01:24:55 +0800

    Implemented skeleton of post routes.

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
-var express = require('express');
-var mongoose = require('mongoose');
-var cors = require('cors');
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
 
 const app = express();
+
+app.use('/posts', postRoutes);
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
