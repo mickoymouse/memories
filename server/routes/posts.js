e802c9ea0ed8d627ac1f8c226d6d7ae82dc0ad66
mickoymouse <mckslvstr@gmail.com>
2021-06-30 01:37:01 +0800

    Added functionality to get and create posts.

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,10 @@
 import express from 'express'
 
+import { getPosts, createPost } from '../controllers/posts.js'
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('This works!');
-});
+router.get('/', getPosts);
+router.post('/', createPost);
 
 export default router;
