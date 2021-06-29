ee18339330cd1e3e753c398e7c9bea0f2d814829
mickoymouse <mckslvstr@gmail.com>
2021-06-30 01:59:53 +0800

    Added boiler plate for form, posts, and post.

---
+++ client/src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+import memories from './images/memories.png';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import useStyles from './styles';
+
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography variant="h2" align="center">Memories</Typography>
+        <img className={classes.image} src={ memories } alt="memories" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" sapcing={ 3 }>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  )
+}
+
+export default App;
