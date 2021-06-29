65a90006e62d446e891b3a2c84c461eeb2e7f08e
mickoymouse <mckslvstr@gmail.com>
2021-06-30 03:20:26 +0800

    Fixed ordering of middleware to enable cors to every domain.

--- server/index.js
+++ server/index.js
@@ -6,11 +6,11 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
-
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = "mongodb+srv://micko:P@55w0rd123@memories.2mocc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
