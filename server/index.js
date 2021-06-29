d076efe636c2ea1e688de45833f58fd6cdf1103d
mickoymouse <mckslvstr@gmail.com>
2021-06-29 11:06:27 +0800

    Initialized mongoose connection.

--- server/index.js
+++ server/index.js
@@ -8,3 +8,15 @@ app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+const CONNECTION_URL = "mongodb+srv://micko:P@55w0rd123@memories.2mocc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)
+))
+.catch(error => console.error(error.message));
+
+mongoose.set('useFindAndModify', false);
